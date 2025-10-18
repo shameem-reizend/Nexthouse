d87a01d433abea93daa80ad31725ede6a756ccc7
shameem-reizend <mohammed.shameem@reizend.ai>
2025-10-18 11:09:36 +0530

    added error handler and routes to app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,9 @@
 import app from "./app";
 import { AppDataSource } from "./config/data-source";
-import { errorHandler } from "./middlewares/errorHandler";
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT;
-
-
-app.use(errorHandler);
 
 AppDataSource.initialize()
 .then(() => {
