d9dc0e664bbc147b072301c5b694014ac2a3fd1f
Alfin Johnson <alfin.johnson@reizend.ai>
2025-10-22 17:23:28 +0530

    invite api

--- backend/src/services/invite.service.ts
+++ backend/src/services/invite.service.ts
@@ -1,12 +1,41 @@
 import { AppDataSource } from "../config/data-source";
 import { Invite } from "../entities/Invite.entity";
+import { ApiError } from "../utils/apiError";
 
 const inviteRepo = AppDataSource.getRepository(Invite);
 export const createInvite = async (event, sender, reciever) => {
-  const invite = await inviteRepo.create({
+
+  // checking whether reciever is already invited for event
+  const existinginvite = await inviteRepo.findOne({
+    where: {
+      event: { event_id: event.event_id },
+      reciever: { user_id: reciever.user_id },
+    },
+    relations: ["event", "sender", "reciever"],
+  });
+
+  if (existinginvite) {
+    throw new ApiError("This Receiver has already invited for this event",409);
+  }
+
+  const invite =  inviteRepo.create({
     event,
     sender,
     reciever,
   });
+
   return await inviteRepo.save(invite);
+};
+
+export const getAllInvites = async () => {
+  return await inviteRepo.find();
+};
+
+export const getAllInvitesByEvent = async (event_id: string) => {
+  const invites = await inviteRepo.find({
+    where: {
+      event: { event_id },
+    },
+  });
+  return invites;
 };
