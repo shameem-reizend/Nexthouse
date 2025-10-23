dcd2a8d37e99344de351abb19e2bb8e69328326e
shameem-reizend <mohammed.shameem@reizend.ai>
2025-10-23 12:10:08 +0530

    design changes and logout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const Home: React.FC = () => {
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/login');
+  }
+
   return (
-    <div>Home</div>
+    <div className='p-5'>
+      <div>Home</div>
+      <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer' onClick={handleLogout}>logout</button>
+    </div>
+    
   )
 }
